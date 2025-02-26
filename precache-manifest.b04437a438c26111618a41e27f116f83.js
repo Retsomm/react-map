656ddc2a96a46e71127191804afc99a6e54864a1
Aria <112182ssss@example.com>
2025-02-26 15:02:34 +0800

    Updates

---
+++ precache-manifest.b04437a438c26111618a41e27f116f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c06aa9962571a0b13e59d138f5eb8e3",
+    "url": "/react-map/index.html"
+  },
+  {
+    "revision": "8646e4b37c76125909ba",
+    "url": "/react-map/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e1739488586cf2300338",
+    "url": "/react-map/static/js/2.86e82c67.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/react-map/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "8646e4b37c76125909ba",
+    "url": "/react-map/static/js/main.f164e59c.chunk.js"
+  },
+  {
+    "revision": "7dbc18e94e38b3b3a265",
+    "url": "/react-map/static/js/runtime~main.0f196493.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-map/static/media/logo.06e73328.svg"
+  }
+]);
